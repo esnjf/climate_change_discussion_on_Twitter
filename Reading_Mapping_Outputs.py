586f110751b0db3d6b14d5a4b011577c2c10ca3e
Ehsan Najafi <43420592+esnjf@users.noreply.github.com>
2021-05-07 11:54:34 -0400

    Update and rename reading_twitter_data.py to Reading_Mapping_Outputs.py

--- Reading_Mapping_Outputs.py
+++ Reading_Mapping_Outputs.py
@@ -2,7 +2,6 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 tweets_data_path = 'C:/Users/Ehsan/PycharmProjects/DataIncubator/data.txt'
 
 tweets_data = []
@@ -13,14 +12,12 @@ for line in tweets_file:
         tweets_data.append(tweet)
     except:
         continue
-
 print len(tweets_data)
 
 tweets = pd.DataFrame()
 tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
 tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
 tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
-
 
 tweets_by_lang = tweets['lang'].value_counts()
 
@@ -32,19 +29,11 @@ ax.set_ylabel('Number of tweets' , fontsize=15)
 ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
 
 tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
-
-
-
-
-
 tweets_by_country = tweets['country'].value_counts()
-
 fig, ax = plt.subplots()
 ax.tick_params(axis='x', labelsize=15)
 ax.tick_params(axis='y', labelsize=10)
 ax.set_xlabel('Countries', fontsize=15)
 ax.set_ylabel('Number of tweets' , fontsize=15)
 ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
-
-
 tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
